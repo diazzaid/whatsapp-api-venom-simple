07b59f9bde4048fb1e25a7835ac4389b01b7a6c3
kang zaid <diaz.zaid20@gmail.com>
2022-02-09 17:27:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,35 +36,8 @@ venom.create(
         logQR: true, // Logs QR automatically in terminal
         //browserWS: 'ws://localhost:3030', // If u want to use browserWSEndpoint
         browserArgs: [
-		'--log-level=3',
-                    '--no-default-browser-check',
-                    '--disable-site-isolation-trials',
-                    '--no-experiments',
-                    '--ignore-gpu-blacklist',
-                    '--ignore-certificate-errors',
-                    '--ignore-certificate-errors-spki-list',
-                    '--disable-gpu',
-                    '--disable-extensions',
-                    '--disable-default-apps',
-                    '--enable-features=NetworkService',
-                    '--disable-setuid-sandbox',
+
                     '--no-sandbox',
-                    // Extras
-                    '--disable-webgl',
-                    '--disable-threaded-animation',
-                    '--disable-threaded-scrolling',
-                    '--disable-in-process-stack-traces',
-                    '--disable-histogram-customizer',
-                    '--disable-gl-extensions',
-                    '--disable-composited-antialiasing',
-                    '--disable-canvas-aa',
-                    '--disable-3d-apis',
-                    '--disable-accelerated-2d-canvas',
-                    '--disable-accelerated-jpeg-decoding',
-                    '--disable-accelerated-mjpeg-decode',
-                    '--disable-app-list-dismiss-on-blur',
-                    '--disable-accelerated-video-decode',
-				    '--single-process', // <- this one doesn't works in Windows
 		], 
     })
   .then((client) => start(client))
